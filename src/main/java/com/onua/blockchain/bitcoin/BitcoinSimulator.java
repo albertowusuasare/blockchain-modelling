d68fb82237d801e8c5019efcf5f7a8022d5664d6
albertowusuasa <owusuasa@grinnell.edu>
2017-09-25 20:21:09 -0400

    Add Logging framework - Add log4j2 config and dependencies

--- src/main/java/com/onua/blockchain/bitcoin/BitcoinSimulator.java
+++ src/main/java/com/onua/blockchain/bitcoin/BitcoinSimulator.java
@@ -2,6 +2,8 @@ package com.onua.blockchain.bitcoin;
 
 import com.onua.blockchain.bitcoin.domain.Transaction;
 import io.reactivex.Observable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 import java.util.ArrayList;
@@ -9,12 +11,14 @@ import java.util.List;
 
 
 public class BitcoinSimulator {
+    private static final Logger logger = LogManager.getLogger(BitcoinSimulator.class);
 
     public interface MyDummyService{
 
     }
 
-    public static void main(String args){
+    public static void main(String [] args){
+        logger.info("Starting simulator");
         List<Transaction> transactions = getDummyTransactions();
         NewTransactionStore newTransactionStore = new NewTransactionStore();
         Observable.fromIterable(transactions).subscribe(newTransactionStore);
