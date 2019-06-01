a9066242fbd4d7f2d5dcef9531723c8ba80cb53a
albertowusuasa <owusuasa@grinnell.edu>
2019-06-01 10:22:08 -0400

    testing vs code commit

--- src/main/java/com/onua/blockchain/bitcoin/CryptographyUtils.java
+++ src/main/java/com/onua/blockchain/bitcoin/CryptographyUtils.java
@@ -3,6 +3,6 @@ package com.onua.blockchain.bitcoin;
 public class CryptographyUtils {
 
     public String signMessage(String message, String privateKey){
-        throw new RuntimeException("Not yet implemented");
+        throw new RuntimeException("Not yet implemented. Will do in a bit");
     }
 }
